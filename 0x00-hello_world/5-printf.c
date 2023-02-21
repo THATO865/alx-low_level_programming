673118a006fa4b77a37333d271d31ce559cad020
THATO865 <pulephetogo771@gmail.com>
2023-02-20 16:48:33 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio>
+#include <stdio.h>
+
 /**
- * Main - prints out a string to stdout
- *
- * Return:0 (success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammer,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

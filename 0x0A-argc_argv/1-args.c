e3bcdd0798b9397268642eefe241ccc9d6f2fbbe
THATO865 <pulephetogo771@gmail.com>
2023-03-13 17:52:31 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,19 +4,21 @@
 
 
 
-/**
-	
- * main - print the number of arguments passed to the program
-
- * @argc: number of arguments
-
- * @argv: array of arguments
-
+ /**
  *
 
- * Return: Always 0 (Success)
 
- */		
+ * main - print the number of arguments passed to the program
+
+ *@argc: number of arguments
+
+ *@argv: array of arguments
+
+
+ *Return: Always 0 (Success)
+
+
+ */
 
 
 int main(int argc, char *argv[])

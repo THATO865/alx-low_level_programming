901886bd13e1efec4f966eb05c9407a86a67dd67
THATO865 <pulephetogo771@gmail.com>
2023-03-23 18:03:01 -0700

    initial commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: 0 or 1
+ * Return:0 or 1
  */
 int _isupper(int c)
 {
@@ -12,4 +12,3 @@ int _isupper(int c)
 
 	else
 	return (0);
-
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -pendantic -werror -wextra -c *.c
+gcc -Wall -Werror -Wextra -pedantic -c *.c
 ar -rc liball.a *.o
 ranlib liball.a

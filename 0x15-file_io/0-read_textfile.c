dff2c7cbe2eb5a9051dae4c0bddfdca13db82c24
THATO865 <pulephetogo771@gmail.com>
2023-07-10 08:38:15 -0700

    input/output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    FILE *fp;
+    char *buffer;
+    ssize_t read_chars;
+
+    if (filename == NULL)
+        return (0);
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    read_chars = fread(buffer, sizeof(char), letters, fp);
+    if (read_chars == 0)
+        return (0);
+
+    printf("%s", buffer);
+
+    fclose(fp);
+    free(buffer);
+
+    return (read_chars);
+}
+
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *
+ *
+ *
+ */
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    FILE *file_from = fopen(argv[1], "r");
+    if (file_from == NULL) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    FILE *file_to = fopen(argv[2], "w");
+    if (file_to == NULL) {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
+
+    char buffer[BUFFER_SIZE];
+    size_t read_size;
+    while ((read_size = fread(buffer, 1, BUFFER_SIZE, file_from)) > 0) {
+        if (fwrite(buffer, 1, read_size, file_to) != read_size) {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+    }
+
+    if (fclose(file_from) != 0) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(file_from));
+        exit(100);
+    }
+
+    if (fclose(file_to) != 0) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(file_to));
+        exit(100);
+    }
+
+    return 0;
+}
+
